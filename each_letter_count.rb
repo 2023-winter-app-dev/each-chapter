5bb5c08002289185fd1b43f08362324c72a27b4a
cwen-booth <cwen@chicagobooth.edu>
2023-01-18 02:22:25 +0000

    HW 1

--- each_letter_count.rb
+++ each_letter_count.rb
@@ -1,7 +1,11 @@
 # Write a program that:
-# 
+#
 # Asks the user to enter a word.
 #  The program should print each letter in the word the number of times it appears in the word.
 # Look in the github README file for example output
 
 p "Enter a word:"
+word = gets.chomp.split("")
+word.each do |letter|
+  p letter + " appears " + word.count(letter).to_s + " times"
+end
